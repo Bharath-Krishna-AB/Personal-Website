e349d33029b3532ef1cde2e8fcf6cb4f7ca2da0b
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2022-01-26 21:06:34 +0530

    setted on scroll change nav color

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ var typed = new Typed('.auto-input',{
     backSpeed:100,
     loop: true
 })
+
+
+var navbar = document.querySelector('.NavBar')
+
+
+addEventListener('scroll',()=>{
+    if(window.scrollY > 300){
+        navbar.style.background = '#485460'
+    }else{
+        navbar.style.background = 'transparent'
+    }
+})
